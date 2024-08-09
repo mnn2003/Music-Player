7f0e1a4cdc1634ddd19493698e0078c207171f9c
Aman Tiwari <88995970+mnn2003@users.noreply.github.com>
2024-08-09 11:00:50 +0530

    Add files via upload

---
+++ src/musicData.ts
@@ -0,0 +1,446 @@
+import { Song } from './types'; // Ensure correct path to types
+
+export const songs: Song[] = [
+
+  {
+    id: '419',
+    title: 'Aasmaan',
+    artist: 'Sanjana Devarajan',
+    coverUrl: '/covers/cover419.jpeg',
+    audioUrl: '/song419.mp3',
+  },
+  {
+    id: '59',
+    title: 'Heeriye ',
+    artist: 'Jasleen Royal, Arijit Singh ',
+    coverUrl: '/covers/cover128.jpg',
+    audioUrl: '/song128.mp3',
+  },
+
+
+  {
+    id: '29',
+    title: 'STAY',
+    artist: 'The Kid LAROI, Justin Bieber',
+    coverUrl: '/covers/cover108.png',
+    audioUrl: '/song108.mp3',
+  },
+
+ 
+ {
+    id: '45',
+    title: 'love nwantiti ',
+    artist: 'CKay',
+    coverUrl: '/covers/cover117.jpeg',
+    audioUrl: '/song117.mp3',
+  },
+
+  {
+    id: '23',
+    title: 'Illuminati',
+    artist: 'Sushin Shyam, Dabzee',
+    coverUrl: '/covers/cover103.jpg',
+    audioUrl: '/song103.mp3',
+  },
+  {
+    id: '400',
+    title: 'Faded',
+    artist: 'Alan Walker',
+    coverUrl: '/covers/cover400.jpeg',
+    audioUrl: '/song400.mp3',
+  },
+
+
+
+  {
+    id: '36',
+    title: 'Starboy',
+    artist: 'The Weeknd, Daft Punk',
+    coverUrl: '/covers/cover113.jpeg',
+    audioUrl: '/song113.mp3',
+  },
+
+
+  {
+    id: '39',
+    title: 'Señorita',
+    artist: 'Shawn Mendes, Camila Cabello',
+    coverUrl: '/covers/cover114.jpg',
+    audioUrl: '/song114.mp3',
+  },
+  {
+    id: '2',
+    title: 'Akhiyaan',
+    artist: 'Mitraz',
+    coverUrl: '/covers/cover2.jpg',
+    audioUrl: '/song7.mp3',
+  },
+  {
+    id: '401',
+    title: "We Don't Talk Anymore",
+    artist: 'Charlie Puth, Selena Gomez',
+    coverUrl: '/covers/cover401.jpeg',
+    audioUrl: '/song401.mp3',
+  },
+
+  {
+    id: '300',
+    title: 'Baarishein',
+    artist: 'Anuv Jain',
+    coverUrl: '/covers/cover4.jpg',
+    audioUrl: '/song12.mp3',
+  },
+
+  {
+    id: '3',
+    title: 'Antariksh',
+    artist: 'Anuv Jain',
+    coverUrl: '/covers/cover3.jpeg',
+    audioUrl: '/song9.mp3',
+  },
+
+  {
+    id: '4',
+    title: 'Chaand Baaliyan',
+    artist: 'Aditya A',
+    coverUrl: '/covers/cover5.jpeg',
+    audioUrl: '/song15.mp3',
+  },
+
+
+
+  {
+    id: '402',
+    title: "Bad Habits",
+    artist: 'Ed Sheeran',
+    coverUrl: '/covers/cover402.jpeg',
+    audioUrl: '/song402.mp3',
+  },
+
+
+  {
+    id: '6',
+    title: 'Faasle',
+    artist: 'Aditya Rikhari',
+    coverUrl: '/covers/cover7.jpg',
+    audioUrl: '/song21.mp3',
+  },
+  {
+    id: '30',
+    title: 'Let Me Love You',
+    artist: 'DJ Snake, Justin Bieber',
+    coverUrl: '/covers/cover109.jpeg',
+    audioUrl: '/song109.mp3',
+  },
+  {
+    id: '31',
+    title: 'Save Your Tears',
+    artist: 'The Weeknd',
+    coverUrl: '/covers/cover110.jpg',
+    audioUrl: '/song110.mp3',
+  },
+  {
+    id: '32',
+    title: 'Believer',
+    artist: 'Imagine Dragons',
+    coverUrl: '/covers/cover111.jpg',
+    audioUrl: '/song111.mp3',
+  },
+
+
+
+
+  {
+    id: '414',
+    title: 'Lost Soul',
+    artist: 'NBSPLV',
+    coverUrl: '/covers/cover414.jpeg',
+    audioUrl: '/song414.mp3',
+  },
+
+
+
+  {
+    id: '34',
+    title: 'On & On',
+    artist: 'Cartoon, Daniel Levi',
+    coverUrl: '/covers/cover112.jpeg',
+    audioUrl: '/song112.mp3',
+  },
+
+  {
+    id: '7',
+    title: 'Farq hai',
+    artist: 'Suzonn',
+    coverUrl: '/covers/cover8.jpg',
+    audioUrl: '/song22.mp3',
+  },
+  {
+    id: '8',
+    title: 'Gul',
+    artist: 'Anuv Jain',
+    coverUrl: '/covers/cover9.jpg',
+    audioUrl: '/song24.mp3',
+  },
+
+  {
+    id: '9',
+    title: 'Husn',
+    artist: 'Anuv Jain',
+    coverUrl: '/covers/cover10.jpg',
+    audioUrl: '/song31.mp3',
+  },
+  {
+    id: '404',
+    title: 'Amplifier',
+    artist: 'Imran Khan',
+    coverUrl: '/covers/cover404.jpeg',
+    audioUrl: '/song404.mp3',
+  },
+
+  {
+    id: '10',
+    title: 'Iraaday',
+    artist: 'Abdul Hannan and Rovalio',
+    coverUrl: '/covers/cover11.jpg',
+    audioUrl: '/song34.mp3',
+  },
+
+  {
+    id: '403',
+    title: 'Shinunoga E-Wa',
+    artist: 'Fujii Kaze',
+    coverUrl: '/covers/cover403.jpeg',
+    audioUrl: '/song403.mp3',
+  },
+
+  {
+    id: '410',
+    title: 'Ek Zindagi',
+    artist: 'Taniskaa Sanghvi, Sachin-Jigar',
+    coverUrl: '/covers/cover410.jpeg',
+    audioUrl: '/song410.mp3',
+  },
+
+  {
+    id: '11',
+    title: 'Junoon',
+    artist: 'Mitraz',
+    coverUrl: '/covers/cover12.jpg',
+    audioUrl: '/song39.mp3',
+  },
+
+  {
+    id: '12',
+    title: 'Kahani Suno ',
+    artist: 'Kaifi Khalil',
+    coverUrl: '/covers/cover13.jpg',
+    audioUrl: '/song41.mp3',
+  },
+
+  {
+    id: '13',
+    title: 'KTMBK',
+    artist: 'Hanita Bhambri and Zaeden',
+    coverUrl: '/covers/cover14.jpg',
+    audioUrl: '/song47.mp3',
+  },
+
+  {
+    id: '412',
+    title: 'Everything at Once',
+    artist: 'Lenka',
+    coverUrl: '/covers/cover412.jpeg',
+    audioUrl: '/song412.mp3',
+  },
+
+  {
+    id: '413',
+    title: 'Left and Right',
+    artist: 'Charlie Puth, Jung Kook, BTS',
+    coverUrl: '/covers/cover413.jpeg',
+    audioUrl: '/song413.mp3',
+  },
+
+
+
+
+
+
+  {
+    id: '14',
+    title: 'Lamhey',
+    artist: 'Anubha Bajaj',
+    coverUrl: '/covers/cover15.jpg',
+    audioUrl: '/song53.mp3',
+  },
+
+
+
+  {
+    id: '17',
+    title: 'Sajni',
+    artist: 'Arijit Singh',
+    coverUrl: '/covers/cover18.jpg',
+    audioUrl: '/song72.mp3',
+  },
+  {
+    id: '405',
+    title: 'Bewafa',
+    artist: 'Imran Khan',
+    coverUrl: '/covers/cover405.jpeg',
+    audioUrl: '/song405.mp3',
+  },
+
+
+  {
+    id: '1',
+    title: 'Akhiyaan Gulaab',
+    artist: 'Mitraz',
+    coverUrl: '/covers/cover1.jpeg',
+    audioUrl: '/song6.mp3',
+  },
+
+
+
+  {
+    id: '43',
+    title: 'Into Your Arms  ',
+    artist: 'Witt Lowry, Ava Max',
+    coverUrl: '/covers/cover116.jpg',
+    audioUrl: '/song116.mp3',
+  },
+
+
+
+  {
+    id: '406',
+    title: 'Kun Faya Kun',
+    artist: 'A.R. Rahman',
+    coverUrl: '/covers/cover406.jpeg',
+    audioUrl: '/song406.mp3',
+  },
+
+
+  {
+    id: '46',
+    title: 'End of Time',
+    artist: 'K-391, Alan Walker, Ahrix',
+    coverUrl: '/covers/cover118.jpg',
+    audioUrl: '/song118.mp3',
+  },
+  {
+    id: '47',
+    title: 'Fortnight',
+    artist: 'Taylor Swift, Post Malone',
+    coverUrl: '/covers/cover119.png',
+    audioUrl: '/song119.mp3',
+  },
+
+
+  {
+    id: '50',
+    title: 'Talking to the Moon',
+    artist: 'Bruno Mars',
+    coverUrl: '/covers/cover121.jpg',
+    audioUrl: '/song121.mp3',
+  },
+
+
+  {
+    id: '409',
+    title: 'Intentions ',
+    artist: 'Justin Bieber, Quavo',
+    coverUrl: '/covers/cover409.jpeg',
+    audioUrl: '/song409.mp3',
+  },
+
+
+
+
+  {
+    id: '55',
+    title: 'Tujhe Kitna Chahne Lage',
+    artist: 'Arijit Singh, Mithoon',
+    coverUrl: '/covers/cover124.webp',
+    audioUrl: '/song124.mp3',
+  },
+  {
+    id: '56',
+    title: 'Paon Ki Jutti',
+    artist: 'Jyoti Nooran, Jaani, Bunny',
+    coverUrl: '/covers/cover125.jpg',
+    audioUrl: '/song125.mp3',
+  },
+
+  {
+    id: '60',
+    title: 'Tum Se Hi',
+    artist: 'Pritam, Mohit Chauhan',
+    coverUrl: '/covers/cover129.jpg',
+    audioUrl: '/song140.mp3',
+  },
+  {
+    id: '61',
+    title: 'O Maahi',
+    artist: 'Pritam, Arijit Singh, Irshad Kamil',
+    coverUrl: '/covers/cover130.jpg',
+    audioUrl: '/song130.mp3',
+  },
+  {
+    id: '416',
+    title: 'The Drum',
+    artist: 'Alan Walker',
+    coverUrl: '/covers/cover416.jpeg',
+    audioUrl: '/song416.mp3',
+  },
+
+  {
+    id: '133',
+    title: 'MIDDLE OF THE NIGHT',
+    artist: 'Elley Duhé',
+    coverUrl: '/covers/cover133.jpg',
+    audioUrl: '/song133.mp3',
+  },
+
+  {
+    id: '65',
+    title: 'Kaise Hua ',
+    artist: 'Vishal Mishra',
+    coverUrl: '/covers/cover135.jpg',
+    audioUrl: '/song135.mp3',
+  },
+
+
+  {
+    id: '408',
+    title: 'The Breakup Song',
+    artist: 'Pritam, Arijit Singh',
+    coverUrl: '/covers/cover408.jpeg',
+    audioUrl: '/song408.mp3',
+  },
+
+  {
+    id: '69',
+    title: 'Tera Hua',
+    artist: 'Atif Aslam',
+    coverUrl: '/covers/cover139.jpg',
+    audioUrl: '/song139.mp3',
+  },
+
+  {
+    id: '71',
+    title: 'Bakhuda Tumhi Ho',
+    artist: 'Atif Aslam',
+    coverUrl: '/covers/cover140.jpeg',
+    audioUrl: '/song129.mp3',
+  },
+
+
+
+];
+
+
+
+
+
