bb765946f50911a6cfa4db31b7d017731a9fe956
Aman Tiwari <88995970+mnn2003@users.noreply.github.com>
2024-08-10 12:34:18 +0530

    Update SongList.tsx

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Song } from '../types';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
@@ -9,14 +9,13 @@ import { useTheme } from '../context/ThemeContext';
 interface SongListProps {
   tracks: Song[];
   onSongClick: (song: Song) => void;
+  currentSong: Song | null; // Accepting currentSong as a prop
 }
 
-const SongList: React.FC<SongListProps> = ({ tracks, onSongClick }) => {
+const SongList: React.FC<SongListProps> = ({ tracks, onSongClick, currentSong }) => {
   const { isDarkMode } = useTheme();
-  const [currentSong, setCurrentSong] = useState<Song | null>(null);
 
   const handleClick = (song: Song) => {
-    setCurrentSong(song);
     onSongClick(song);
   };
 
@@ -44,19 +43,24 @@ const SongList: React.FC<SongListProps> = ({ tracks, onSongClick }) => {
       {tracks.map((track) => (
         <motion.div
           key={track.id}
-          className={`p-4 rounded-lg flex items-center space-x-4 cursor-pointer transition-all duration-300 ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'} ${currentSong && currentSong.id === track.id ? 'active-song' : ''}`}
+          className={`p-4 rounded-lg flex items-center space-x-4 cursor-pointer transition-all duration-300 ${
+            isDarkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'
+          } ${currentSong && currentSong.id === track.id ? 'active-song' : ''}`}
           onClick={() => handleClick(track)}
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
-
         >
-
-
           <Image src={track.coverUrl} alt={track.title} width={64} height={64} className="h-16 w-16 rounded-lg" />
           <div>
             <p className={isDarkMode ? 'text-white' : 'text-gray-900'}>{track.title}</p>
-            <p className={`${currentSong && currentSong.id === track.id ? 'text-gray-900' : isDarkMode ? 'text-gray-400' : 'text-gray-500'}`}>{track.artist}</p>
+            <p
+              className={`${
+                currentSong && currentSong.id === track.id ? 'text-gray-900' : isDarkMode ? 'text-gray-400' : 'text-gray-500'
+              }`}
+            >
+              {track.artist}
+            </p>
           </div>
         </motion.div>
       ))}
